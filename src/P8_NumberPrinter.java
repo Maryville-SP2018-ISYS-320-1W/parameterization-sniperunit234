07b018b005bced781d3fddfc8d3feb46ebb28ae4
sniperunit234 <37313753+sniperunit234@users.noreply.github.com>
2018-03-23 11:05:59 -0400

    printer
    Fun and challenging

--- src/P8_NumberPrinter.java
+++ src/P8_NumberPrinter.java
@@ -1,14 +1,14 @@
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s): Gabriel Lopez	
+	Date: 03/23/2018
 */
 
 public class P8_NumberPrinter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		for (int i = 1; i <=5; i++) {
+			System.out.print("["+i+"]"+" ");
+		}
 	}
-
-}
+}	
